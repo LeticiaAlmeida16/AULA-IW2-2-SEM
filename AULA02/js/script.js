c8e370d2cc2089d6cc9970c204f7107190413d53
LeticiaAlmeida16 <leticia.almeidaaa2008@gmail.com>
2024-04-28 17:06:48 -0300

    continuando

--- AULA02/js/script.js
+++ AULA02/js/script.js
@@ -1,13 +1,21 @@
 const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
 
 const jump = () => {
     mario.classList.add('jump')
 
     setTimeout(() => {
-    
         mario.classList.remove('jump');
-    
     }, 500);
 }
+
+const loop = setInterval(() => {
+
+    const pipePosicao = pipe.offsetLeft;
+    if (pipePosicao = 120){
+        pipe.st
+    }
+
+}, 10)
 
 document.addEventListener('keydown', jump);

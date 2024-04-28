6b8b62edbd94191190ee51af24bd071eafc8cfb4
LeticiaAlmeida16 <leticia.almeidaaa2008@gmail.com>
2024-04-28 17:59:43 -0300

    JOGO-MARIO

--- AULA02/js/script.js
+++ AULA02/js/script.js
@@ -1,21 +1,35 @@
-const mario = document.querySelector('.mario');
-const pipe = document.querySelector('.pipe');
+const mario = document.querySelector('.mario')
+const pipe = document.querySelector('.pipe')
 
 const jump = () => {
     mario.classList.add('jump')
 
     setTimeout(() => {
-        mario.classList.remove('jump');
+        mario.classList.remove('jump')
     }, 500);
 }
 
 const loop = setInterval(() => {
 
-    const pipePosicao = pipe.offsetLeft;
-    if (pipePosicao = 120){
-        pipe.st
+    const pipePosicao = pipe.offsetLeft
+    const marioPosicao = +window.getComputedStyle(mario).bottom.replace('px', '')
+
+    console.log(marioPosicao)
+
+    if (pipePosicao <= 120 && pipePosicao > 0 && marioPosicao < 80){
+        pipe.style.animation = 'none'
+        pipe.style.left = `${pipePosicao}px`
+
+        mario.style.animation = 'none'
+        mario.style.bottom = `${marioPosicao}px`
+
+        mario.src = './img/game-over.png'
+        mario.style.width = '75px'
+        mario.style.marginLeft = '47px'
+
+        clearInterval(loop)
     }
 
 }, 10)
 
-document.addEventListener('keydown', jump);
+document.addEventListener('keydown', jump)
